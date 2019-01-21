35f230763f08ac574383d4331f8eac05c8e2b676
razeej21 <razeej21@in.ibm.com>
2019-01-21 14:11:57 +0530

    Modified commit

--- src/main/java/com/stackroute/MuzixApplication/repository/TrackRepository.java
+++ src/main/java/com/stackroute/MuzixApplication/repository/TrackRepository.java
@@ -4,10 +4,9 @@ import com.stackroute.MuzixApplication.domain.Track;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 
